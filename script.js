3c8c25fdd1d9ef771d1a099b6ff402deeba9ccf6
xAbliv <99667142+xAbliv@users.noreply.github.com>
2023-02-21 07:50:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-// script
+const phrases = ["xabliv", "HTML, CSS, SCSS, JS, react.js"];
+const textElement = document.querySelector('.text');
+const cursor = document.querySelector('.cursor');
+let phraseIndex = 0;
+let letterIndex = 0;
+let typingInterval;
+
+
+function typeText() {
+  const currentPhrase = phrases[phraseIndex];
+  textElement.textContent += currentPhrase[letterIndex];
+  letterIndex++;
+  if (letterIndex >= currentPhrase.length) {
+    clearInterval(typingInterval);
+    if (phraseIndex === 0) {
+      setTimeout(() => {
+        textElement.textContent = "";
+        phraseIndex++;
+        letterIndex = 0;
+        typingInterval = setInterval(typeText, 100);
+      }, 1000);
+    } else {
+      phraseIndex++;
+      letterIndex = 0;
+      if (phraseIndex < phrases.length) {
+        setTimeout(() => {
+          typingInterval = setInterval(typeText, 100);
+        }, 1000);
+      }
+    }
+  }
+}
+
+
+typingInterval = setInterval(typeText, 100);
+
+
+setInterval(() => {
+  cursor.style.opacity = cursor.style.opacity === '0' ? '1' : '0';
+}, 500);
